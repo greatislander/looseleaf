5bdc3abe127018983fe2a075a3285558bf4d930b
Ned Zimmerman <ned@bight.dev>
2021-02-12 11:56:32 -0400

    chore: add JavaScript linting, CI, and Dependabot configuration

--- fractal.config.js
+++ fractal.config.js
@@ -1,16 +1,14 @@
-'use strict';
+const fractal = module.exports = require("@frctl/fractal").create();
 
-const fractal = module.exports = require('@frctl/fractal').create();
-
-fractal.set('project.title', 'Looseleaf Prototyping Library');
-fractal.components.engine('@frctl/nunjucks'); 
-fractal.components.set('ext', '.njk');
-fractal.components.set('path', __dirname + '/src/components');
-fractal.web.set('server.sync', true);
-fractal.web.set('server.syncOptions', {
-    files: ['dist/**/*']
+fractal.set("project.title", "Looseleaf Prototyping Library");
+fractal.components.engine("@frctl/nunjucks");
+fractal.components.set("ext", ".njk");
+fractal.components.set("path", __dirname + "/src/components");
+fractal.web.set("server.sync", true);
+fractal.web.set("server.syncOptions", {
+    files: ["dist/**/*"]
 });
-fractal.web.set('static.path', __dirname + '/dist');
-fractal.web.set('static.mount', 'assets');
-fractal.web.set('builder.dest', __dirname + '/build');
-fractal.docs.set('path', __dirname + '/src/docs');
+fractal.web.set("static.path", __dirname + "/dist");
+fractal.web.set("static.mount", "assets");
+fractal.web.set("builder.dest", __dirname + "/build");
+fractal.docs.set("path", __dirname + "/src/docs");
