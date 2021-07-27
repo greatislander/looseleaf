f4ad1115b71881061cd5d5e7cacf743e12bd20d3
Ned Zimmerman <ned@bight.dev>
2021-07-27 16:00:37 -0300

    chore(docs): use Doxray to extract docs directly from CSS source code (#72)

--- fractal.config.js
+++ fractal.config.js
@@ -1,7 +1,14 @@
 const fractal = module.exports = require("@frctl/fractal").create();
+const doxray = require("doxray");
+const docs = doxray(["src/css/abstracts/_properties.css"]);
+const nunj = require("@frctl/nunjucks")({
+    globals: {
+        docs
+    }
+});
 
 fractal.set("project.title", "Looseleaf Framework");
-fractal.components.engine("@frctl/nunjucks");
+fractal.components.engine(nunj);
 fractal.components.set("ext", ".njk");
 fractal.components.set("path", __dirname + "/src/components");
 fractal.web.set("server.sync", true);
@@ -12,3 +19,4 @@ fractal.web.set("static.path", __dirname + "/dist");
 fractal.web.set("static.mount", "assets");
 fractal.web.set("builder.dest", __dirname + "/build");
 fractal.docs.set("path", __dirname + "/src/docs");
+fractal.docs.engine(nunj);
